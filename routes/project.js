541e30451b9098063a3dda462a5b660fa2d81a3b
hanzzeh@gmail.com <hanzzeh@gmail.com>
2017-02-02 05:20:45 -0800

    done

--- routes/project.js
+++ routes/project.js
@@ -2,6 +2,7 @@ exports.viewProject = function (req, res) {
     //controller code
     var name = req.params.name;
     console.log("Proj name: " + name);
+    
     res.render('project', {
         'projectName': name
     }
